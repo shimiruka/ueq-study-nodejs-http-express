e9e84cd03c9b9e79a7b7eff18b2e0fbababdd8d8
shimiruka <iruka374@gmail.com>
2024-03-27 14:13:24 +0000

    課題のコードを作成

--- express.js
+++ express.js
@@ -1,3 +1,17 @@
 const port = 3001;
 
 // expressでWebサーバーを実装
+import express from "express"
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.get("/hello", (req, res) => {
+    res.json({ message: "hello" })
+})
+
+app.use(express.static("public"));
+
+app.listen(port)
--- http.js
+++ http.js
@@ -2,3 +2,13 @@ const hostname = "localhost";
 const port = 3000;
 
 // httpモジュールでWebサーバーを実装
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World");
+});
+server.listen({
+    host: hostname,
+    port
+});
